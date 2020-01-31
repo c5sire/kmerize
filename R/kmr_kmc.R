ad476edcd176be93d32f4ba1f511a02c4c84b089
Simon, Reinhard <reinhard.simon@wur.nl>
2020-01-31 14:03:38 +0100

    ctd refac

--- R/kmr_kmc.R
+++ R/kmr_kmc.R
@@ -1,8 +1,10 @@
-kmr_kmc <- function(in_files, out_file, k = 11, 
+kmr_kmc <- function(in_files, 
+                    out_file, 
+                    k = 11, 
                     m = 12, # RAM
-                    sm = TRUE, # strict memory mode
+                    sm = FALSE, # strict memory mode
                     p = 9, # signature length: 5:11
-                    f = "fa", # format: fa, fm , fq
+                    f = "a", # format: fa, fm , fq
                     ci = 2, #minim k occurrence
                     cx = 1e9, # max k occ
                     cs = 255, # max counter
@@ -12,12 +14,25 @@ kmr_kmc <- function(in_files, out_file, k = 11,
                     t = parallel::detectCores(), # number of cores
                     sf = 2, # number of threads for FASTQ reading
                     sp = 2, # number of splitting threads
-                    sr = t # number of threads for second stage
+                    sr = 2, # number of threads for second stage
+                    v = FALSE # verbose mode; use to turn off 
 ) {
   # check if files exists
   
-  # check paramater values
+  # check parameter values
   
-  # form strings; omit the lesser used ones
+  # if more than input file use a temp archive to store the list
+  
+  # form parameter strings; omit the lesser used ones: use sprintf
+  
+  params <- " -k%i -m%i -p%i -f%s -ci%i -cx%i -cs%i -n%i -t%i -sf%i -sp%i -sr%i"
+  params <- sprintf(params, k, m, p, f, ci, cx, cs, n, t, sf, sp, sr)
+  
+  if(sm) params <- paste(params, "-sm")
+  if(b) params <- paste(params, "-b")
+  if(r) params <- paste(params, "-r")
+  if(v) params <- paste(params, "-v")
+  
+  # execute as promise!
   
 }
