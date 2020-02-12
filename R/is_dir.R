bb8bbd797c517a48b4786c55c0fe6819f2e92972
c5sire <rsimon64@gmail.com>
2020-02-12 04:06:01 +0100

    better check

--- R/is_dir.R
+++ R/is_dir.R
@@ -1,3 +1,9 @@
 is_dir <- function(apath) {
-  all(dir.exists(dirname(apath))) && length(apath) == 1 && file.info(apath)[["isdir"]]
+
+  all(
+    file.exists(apath),
+    all(dir.exists(dirname(apath))),
+    length(apath) == 1,
+    file.info(apath)[["isdir"]]
+  )
 }
