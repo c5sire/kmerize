40a320a70ff9e327647ef12898eb3261004fb0fd
c5sire <rsimon64@gmail.com>
2020-02-24 14:22:00 +0100

    Configuring vignette for travis (no built because kmc3 will not be installed on CI for the moment).

--- doc/Basic.R
+++ doc/Basic.R
@@ -3,8 +3,13 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+Sys.setenv("NOT_CRAN" = "true")
+
 
 ## ----setup--------------------------------------------------------------------
+testthat::skip_on_cran()
+testthat::skip_on_ci()
+
 suppressPackageStartupMessages(
   library(kmerize)
 )
@@ -19,6 +24,8 @@ out_db <- paste0(out_file, c(".kmc_pre", ".kmc_suf"))
 
 
 ## -----------------------------------------------------------------------------
+testthat::skip_on_cran()
+testthat::skip_on_ci()
 kmer_path = kmr_count(fq, out_file, 
                   k = k, 
                   f = "q" # file input format is fastq
@@ -26,26 +33,21 @@ kmer_path = kmr_count(fq, out_file,
 
 
 ## -----------------------------------------------------------------------------
+testthat::skip_on_cran()
+testthat::skip_on_ci()
 kp <- kmr_write_tab(kmer_path)
 
-## -----------------------------------------------------------------------------
 kmers <- kmr_read_tab(kp)
 head(kmers)
 
 ## -----------------------------------------------------------------------------
 # Getting the DNA
-suppressPackageStartupMessages(
-  library(BSgenome.phix174.NCBI.NC001422)
-)
+testthat::skip_on_cran()
+testthat::skip_on_ci()
 
-phi <- BSgenome.phix174.NCBI.NC001422::BSgenome.phix174.NCBI.NC001422
-dna <- phi$NC_001422.1
-
-# DNA has to be available in a FASTA file for the kmer_response function.
-# So it needs to be converted and stored.
-fp <- file.path(tempdir(), "dna.fasta")
-
-Biostrings::writeXStringSet(DNAStringSet(dna), fp)
+fp <- system.file("testdata/phix174.fasta", 
+                  package = "kmerize")
+dna <- Biostrings::readDNAStringSet(fp)
 
 k <- as.integer(seq(3, 25, 2)) # currently k explicitly as integer sequence
 res <- kmr_response(fp, k, fmt = "m")
@@ -53,6 +55,9 @@ res
 
 
 
-## ----fig.width=7--------------------------------------------------------------
-kmr_plot_response(res, ref_k = 9, max_y = length(dna))
+## ----fig.width=7, warning=FALSE-----------------------------------------------
+testthat::skip_on_cran()
+testthat::skip_on_ci()
+
+kmr_plot_response(res, ref_k = 9, max_y = Biostrings::width(dna))
 
